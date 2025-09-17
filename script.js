4542d6e1c71d507ae57d98dd391f76d1001cb18d
Vikas Rana <38876915+toolblaster@users.noreply.github.com>
2025-09-17 16:26:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -329,7 +329,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <span class="text-2xl">${rhyme.icon || '🎶'}</span>
                     <span class="font-semibold text-brand-dark dark:text-white">${rhyme.title}</span>
                 </div>
-                <button class="p-2 rounded-full hover:bg-red-100 dark:hover:bg-red-800 text-red-500" data-rhyme-id="${rhyme.id}" data-action="remove" title="Remove from playlist">
+                <button class="p-2 rounded-full hover:bg-red-100 dark:hover:bg-red-800 text-red-500" data-rhyme-id="${rhyme.id}" data-action="remove" title="Remove from playlist" aria-label="Remove ${rhyme.title} from playlist">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 pointer-events-none" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" /></svg>
                 </button>
             `;
